a2d969f281e7fac43dc3b7bc077dc04de49c73b0
Joshua Kunst <jbkunst@gmail.com>
2022-04-22 15:38:33 -0400

    Update data.R

--- R/data.R
+++ R/data.R
@@ -1,14 +1,16 @@
 #' Estaciones agromet
 #'
-#'
 #' @section Variables:
 #'
-#' \itemize{
-#'
-#'  \item \code{ema}: Id.
-#'  \item \code{name}: Name.
-#'  \item \code{heigth}: Heigth.
-#'
+#' \itemize{#'
+#'    \item \code{ ema } : ema.
+#'    \item \code{ institucion } : institucion.
+#'    \item \code{ nombre_ema } : nombre_ema.
+#'    \item \code{ comuna } : comuna.
+#'    \item \code{ region } : region.
+#'    \item \code{ latitud } : latitud.
+#'    \item \code{ longitud } : longitud.
+#'    \item \code{ fecha_de_alta } : fecha_de_alta.
 #' }
 #'
 #' @docType data
@@ -16,3 +18,21 @@
 #' @usage estaciones_agromet
 #' @format A \code{data frame} with 417  observations and 8 variables.
 "estaciones_agromet"
+
+#' Variables_agromet
+#'
+#' @section Variables:
+#'
+#' \itemize{
+#'
+#'  \item \code{columna}: Nombre de la columna.
+#'  \item \code{unit}: Unidad de medida.
+#'  \item \code{description}: Descripción de la columna.
+#'
+#' }
+#'
+#' @docType data
+#' @name variables_agromet
+#' @usage variables_agromet
+#' @format A \code{data frame} with 12 observations and 3 variables.
+"variables_agromet"
