050f6e404d26555e038bf9ddad8788ae58520c4a
Joshua Kunst <jbkunst@gmail.com>
2022-04-23 00:08:27 -0400

    Update estaciones_agromet.R

--- data-raw/estaciones_agromet.R
+++ data-raw/estaciones_agromet.R
@@ -8,7 +8,6 @@ estaciones_agromet <- janitor::clean_names(estaciones_agromet)
 
 usethis::use_data(estaciones_agromet, overwrite = TRUE)
 
-
 stringr::str_glue(
   "\t\\item \\code{{ {el} }} : {el}.",
   el = names(agrometR::estaciones_agromet)
